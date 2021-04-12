2726803a72a3b532b3eab7d4eac14a9dbfdfdf92
Lucas Melo <lucasmfs21@gmail.com>
2021-04-12 12:45:05 -0300

    finalizada a parte de integração com banco de daods faunadb e stripe

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,18 +1,22 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/client'
+
 import styles from './styles.module.scss'
 
 export function SignInButton() {
 
-    const isUserLoggedIn = true;
+    const [session] = useSession();
 
-    return isUserLoggedIn ? (
+
+    return session ? (
         <button
             type="button"
             className={styles.signInButton}
+            onClick={() => signOut()}
         >
             <FaGithub color="#04d361" />
-            Lucas Melo
+            {session.user.name}
             <FiX
                 color="#737380"
                 className={styles.closeIcon}
@@ -22,6 +26,7 @@ export function SignInButton() {
         <button
             type="button"
             className={styles.signInButton}
+            onClick={() => signIn('github')}
         >
             <FaGithub color="#eba417" />
             Sign in with Github
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
 import Head from 'next/head';
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
+
 import { SubscribeButton } from '../components/SubscribeButton';
-import styles from './home.module.scss'
 import { stripe } from '../services/stripe';
 
-
-
+import styles from './home.module.scss'
 interface HomeProps {
   product: {
     priceId: string;
@@ -37,7 +36,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1Ib4raFH1kVcFDp5xZMYnqWi', {
     expand: ['product']
   });
@@ -54,7 +53,8 @@ export const getServerSideProps: GetServerSideProps = async () => {
 
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24, //24 hours
 
   }
 }
